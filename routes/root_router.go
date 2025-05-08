b0b806675290a94c516fc78cb30f29c72beb9f17
Drey <KameiKojirou@gmail.com>
2025-05-08 16:02:27 -0600

    remove unused comment

--- routes/root_router.go
+++ routes/root_router.go
@@ -10,6 +10,5 @@ func RootRouter() {
   rootMux.Handle("/api/", http.StripPrefix("/api", APIRouter()))
   rootMux.Handle("/", SPARouter("frontend/dist", "index.html"))
 
-  // 6) Start your server with the single handler:
   http.ListenAndServe(":1323", rootMux)
 }
