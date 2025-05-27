e7d4e67589f9edfa21ede9e0924f5ff9cf53bec1
Drey <KameiKojirou@gmail.com>
2025-05-27 12:49:13 -0600

    fix spa and embed in golang net/http

--- routes/root_router.go
+++ routes/root_router.go
@@ -2,13 +2,14 @@ package routes
 
 import (
   "net/http"
+  "main/assets"
 )
 
 func RootRouter() {
   rootMux := http.NewServeMux()
 
   rootMux.Handle("/api/", http.StripPrefix("/api", APIRouter()))
-  rootMux.Handle("/", SPARouter("frontend/dist", "index.html"))
+  rootMux.Handle("/", assets.SPAHandler("index.html"))
 
   http.ListenAndServe(":1323", rootMux)
 }
