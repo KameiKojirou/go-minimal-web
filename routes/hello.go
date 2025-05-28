553bf5c595f8d2f4db3cedd5906ca8979c7cb7f9
Drey <KameiKojirou@gmail.com>
2025-05-28 13:07:39 -0600

    responding with JSON

--- routes/hello.go
+++ routes/hello.go
@@ -1,15 +1,26 @@
 package routes
 
 import (
-	"fmt"
+	"encoding/json"
+	// "fmt"
 	"net/http"
 )
 
+type HelloNameModel struct {
+	Name string `json:"name"`
+	Thing string `json:"thing"`
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello")
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"msg": "Hello, World!"})
 }
 
 func HelloName(w http.ResponseWriter, r *http.Request) {
 	name :=  r.PathValue("name")
-	fmt.Fprintf(w, "Hello %s", name)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	res := HelloNameModel{Name: name, Thing: "thing"}
+	json.NewEncoder(w).Encode(res)
 }
